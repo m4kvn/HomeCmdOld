5da37e7da774d39f49abfbeebd8e5c0d65cbdd58
MasahiroSaito <fun.masahiro@gmail.com>
2016-04-12 22:03:02 +0900

    作った SQLite をインスタンス化してロードするメソッドを追加

--- src/main/java/com/Nepian/HomeCmd/Main.java
+++ src/main/java/com/Nepian/HomeCmd/Main.java
@@ -2,16 +2,23 @@ package com.Nepian.HomeCmd;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.Nepian.HomeCmd.Database.SQLite;
+
 public class Main extends JavaPlugin {
 	public static JavaPlugin plugin;
+	public static SQLite sqlite;
 
 	@Override
 	public void onEnable() {
 		plugin = this;
+		sqlite = new SQLite(plugin);
+
 		SerializationClassRegister.load();
 		CommandManager.load(plugin);
 		PlayerdataManager.load(FileManager.FOLDER_USERS);
 		ListenerManager.load(plugin);
+
+		sqlite.load();
 	}
 
 	@Override
