82c4ed65b33682568839f0b2478d48ff8eb7179a
MasahiroSaito <b1013236@fun.ac.jp>
2016-04-13 10:51:58 +0900

    MySQLite のロード処理とクローズ処理を Main に追加

--- src/main/java/com/Nepian/HomeCmd/Main.java
+++ src/main/java/com/Nepian/HomeCmd/Main.java
@@ -2,27 +2,23 @@ package com.Nepian.HomeCmd;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.Nepian.HomeCmd.Database.SQLite;
-
 public class Main extends JavaPlugin {
 	public static JavaPlugin plugin;
-	public static SQLite sqlite;
 
 	@Override
 	public void onEnable() {
 		plugin = this;
-		sqlite = new SQLite(plugin);
 
+		MySQLite.load(FileManager.FILE_SQLITE);
 		SerializationClassRegister.load();
 		CommandManager.load(plugin);
 		PlayerdataManager.load(FileManager.FOLDER_USERS);
 		ListenerManager.load(plugin);
-
-		sqlite.load();
 	}
 
 	@Override
 	public void onDisable() {
 		PlayerdataManager.save(FileManager.FOLDER_USERS);
+		MySQLite.close();
 	}
 }
