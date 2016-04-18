57c5da72c2e16f03df005ba1c3d193813ddd012c
MasahiroSaito <b1013236@fun.ac.jp>
2016-04-18 20:09:16 +0900

    SQLite のデータを管理するクラスを作成

---
+++ src/main/java/com/Nepian/HomeCmd/SQLiteManager.java
@@ -0,0 +1,38 @@
+package com.Nepian.HomeCmd;
+
+import java.io.File;
+
+import com.Nepian.HomeCmd.Util.SQLite;
+
+public class SQLiteManager {
+	private static String tableName = "locations";
+	private static SQLite data;
+	
+	public static void load(File file) {
+		data = new SQLite(file);
+		createTable();
+	}
+	
+	private static void createTable() {
+		String token = "create table " + tableName + " ("
+				+ "player_uuid, player_name, home_name, world_uuid, x, y, z, yaw, pitch)";
+		if (!data.executeUpdate(token)) {
+			Logger.failed("Could not create a table (&6" + tableName + "&r)");
+		}
+		Logger.success("Created a table (&6" + tableName + "&r)");
+	}
+//	
+//	public static void insert(OfflinePlayer offlinePlayer, String homeName, Location home) {
+//		String playerUidStr = offlinePlayer.getUniqueId().toString();
+//		String playerName = offlinePlayer.getName();
+//		
+//		data.executeUpdate("insert into " + tableName + " values()");
+//	}
+	
+	public static void close() {
+		if (!data.close()) {
+			Logger.failed("Could not close a SQLite file (&6" + data.getFile().getName() + "&r)");
+		}
+		Logger.success("Closed a SQLite file (&6" + data.getFile().getName() + "&r)");
+	}
+}
