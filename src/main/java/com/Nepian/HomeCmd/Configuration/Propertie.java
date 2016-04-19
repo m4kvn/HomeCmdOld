f5b5b3ce012a3448be54b01fef3349b8d0a16219
MasahiroSaito <b1013236@fun.ac.jp>
2016-04-19 16:39:58 +0900

    Propertie に デフォルホームのホームの名前 "-default" をデータベース登録用に追加

--- src/main/java/com/Nepian/HomeCmd/Configuration/Propertie.java
+++ src/main/java/com/Nepian/HomeCmd/Configuration/Propertie.java
@@ -4,4 +4,5 @@ public class Propertie {
 	public static final String OLD_HOME_DATA_FILE_NAME = "home_data.yml";
 	public static final String USER_DATA_FOLDER_NAME = "users";
 	public static final String SQLITE_FILE_NAME = "data.db";
+	public static final String DEFAULT_HOME_NAME = "-default";
 }
